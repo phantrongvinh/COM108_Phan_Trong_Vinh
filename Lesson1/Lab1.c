43a32b7b37bd6609db5d4c5749c9592abc6f440a
PhanTrongVinh <1phantrongvinh98@gmail.com>
2026-01-09 17:10:24 +0700

    first commit for lab1 and demo

---
+++ Lesson1/Lab1.c
@@ -0,0 +1,11 @@
+// Standard input-output header file
+#include <stdio.h>
+
+// Hàm main
+int main()
+{
+    // In ra màn hình terminal
+    printf("Hello, World!\n");
+    // Trả về 0 báo hiệu chương trình kết thúc thành công
+    return 0;
+}
---
+++ Lesson1/hello.c
@@ -0,0 +1,12 @@
+// Standard input-output header file
+#include <stdio.h>
+
+// Hàm main
+int main()
+{
+    // In ra màn hình tên và mssv
+    printf("Tên: Phan Trọng Vinh\n"
+           "MSSV: PS49909\n");
+    // Trả về 0 báo hiệu chương trình kết thúc thành công
+    return 0;
+}
