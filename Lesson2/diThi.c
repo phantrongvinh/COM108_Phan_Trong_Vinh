c54b4e1578aca089fcfb46c5a1e90f1d83b6ad5c
PhanTrongVinh <1phantrongvinh98@gmail.com>
2026-01-15 10:27:16 +0700

    Hoàn thành demo về sơ đồ khối và thực hiện code

---
+++ Lesson2/diThi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    // Khai báo biến số buổi vắng và điểm trung bình
+    int soBuoiVang = 0;
+    float diemTB = 0.00;
+
+    // Nhập số buổi vắng và điểm trung bình
+    printf("Nhập số buổi vắng: ");
+    scanf("%d", &soBuoiVang);
+    printf("Nhập điểm trung bình: ");
+    scanf("%f", &diemTB);
+
+    // Xét điều kiện số buổi vắng ít hơn 4 VÀ điểm trung bình đạt ít nhất 5 điểm được dự thi, ngược lại
+    if (soBuoiVang < 4 && diemTB >= 5.00)
+    {
+        printf("Sinh viên đủ điều kiện dự thi");
+    }
+    else
+    {
+        printf("Sinh viên phải đăng ký học lại");
+    }
+}
